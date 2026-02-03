29b8a28f32c2bddaf7be01a1f2c6e333d86c262f
Nikita <ne60201@gmail.com>
2026-02-03 09:41:44 +0500

    Responsive chain and centered input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,39 @@
 import "./css/Home.css";
 import Chain from "../assets/chain.svg"; // Иконка левой цепи
 
+import Footer from "./components/Footer";
+
 const Home = () => {
   return (
     <div className="krch">
       <div className="krch-content">
-        <h1>KRCH.IO</h1>
+        <h1 className="logo">KRCH.IO</h1>
         <p>Make links grate again</p>
         
         {/* Блок с пошаговой инструкцией */}
         <div className="steps">
           
           {/* Поле ввода с цепями по бокам */}
-          <div className="input-with-chains">
-            <img src={Chain} alt="left chain" className="chain-left" />
+          {/* <div className="input-with-chains">
             <div className="input-container">
-              <input 
-                type="url" 
-                placeholder="Тут могла быть ваша длинная, угрюмая и скучная ссылка..."
-              />
+            <input 
+            type="url" 
+            placeholder="Тут могла быть ваша длинная, угрюмая и скучная ссылка..."
+            />
             </div>
             <img src={Chain} alt="right chain" className="chain-right" />
+            </div> */}
+          <div className="input-with-chains">
+            <div className="chain-bg" />
+            <div className="input-link">
+              <input type="text" />
+            </div>
           </div>
-          
         </div>
         
         <button className="krch-link">Короче!</button>
       </div>
+      <Footer />
     </div>  
   );
 };
---
+++ src/pages/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./css/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <h1>Что такое krch?</h1>
+    </footer>
+  );
+};
+
+export default Footer;
